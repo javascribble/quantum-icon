b729f101982de0225730cc637d017944a35115e0
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-23 11:10:53 -0400

    remove height and width sizing

--- source/templates/icon.js
+++ source/templates/icon.js
@@ -5,17 +5,9 @@ const html = `
 `;
 
 const css = `
-:host {
-    display: inline-block;
-}
-
 span {
     color: var(--icon-color, inherit);
-    line-height: var(--icon-size, normal);
-    width: var(--icon-size, 1em);
-    display: inline-block;
     position: relative;
-    text-align: center;
 }
 `;
 
