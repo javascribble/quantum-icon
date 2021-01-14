0a38486ed5cb35d60de30f489a80a26595757a1f
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-01-14 15:58:30 -0500

    reimplement private member

--- source/elements/icon.js
+++ source/elements/icon.js
@@ -2,16 +2,24 @@ import { Component, template, define } from '../import.js';
 import html from '../templates/icon.js';
 
 export class Icon extends Component {
+    #icon;
+
+    constructor() {
+        super();
+
+        this.#icon = this.shadowRoot.querySelector('span');
+    }
+
     static template = template(html);
 
     static get observedAttributes() { return ['code', 'offset']; }
 
     codeAttributeChanged(attribute, previousValue, currentValue) {
-        this.shadowRoot.querySelector('span').innerHTML = `&${currentValue};`;
+        this.#icon.innerHTML = `&${currentValue};`;
     }
 
     offsetAttributeChanged(attribute, previousValue, currentValue) {
-        this.shadowRoot.querySelector('span').style.top = currentValue;
+        this.#icon.style.top = currentValue;
     }
 }
 
