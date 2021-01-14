85cc1427e14a5d118904e1d5d64925917d8f2576
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-01-14 15:55:12 -0500

    remove private field

--- source/elements/icon.js
+++ source/elements/icon.js
@@ -2,24 +2,16 @@ import { Component, template, define } from '../import.js';
 import html from '../templates/icon.js';
 
 export class Icon extends Component {
-    #icon;
-
-    constructor() {
-        super();
-
-        this.#icon = this.shadowRoot.querySelector('span');
-    }
-
     static template = template(html);
 
     static get observedAttributes() { return ['code', 'offset']; }
 
     codeAttributeChanged(attribute, previousValue, currentValue) {
-        this.#icon.innerHTML = `&${currentValue};`;
+        this.shadowRoot.querySelector('span').innerHTML = `&${currentValue};`;
     }
 
     offsetAttributeChanged(attribute, previousValue, currentValue) {
-        this.#icon.style.top = currentValue;
+        this.shadowRoot.querySelector('span').style.top = currentValue;
     }
 }
 
